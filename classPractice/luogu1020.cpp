a88dc32570dd7fce001dde3805801c72a5d4fab8
Qeryu <m15265723639@163.com>
2021-10-13 21:53:00 +0800

    拓展1：导弹拦截

---
+++ classPractice/luogu1020.cpp
@@ -0,0 +1,34 @@
+#include <algorithm>
+#include <cstdio>
+#include <iostream>
+using namespace std;
+#define N 100005
+int n, a[N], dpup[N], dpdown[N], ans1, ans2;
+// dpup 最长不上升子序列，nlogn做法
+// dpdown 最长上升子序列，nlogn做法
+
+int main() {
+    // freopen("../test.in", "r", stdin);
+    while (scanf("%d", a + (n++)) != EOF)
+        ;
+    n--;
+    dpup[ans1++] = a[0];
+    dpdown[ans2++] = a[0];
+    for (int i = 1; i < n; ++i) {
+        if (dpup[ans1 - 1] >= a[i]) {
+            dpup[ans1++] = a[i];
+        } else {
+            int p = upper_bound(dpup, dpup + ans1, a[i], greater<int>()) - dpup;
+            dpup[p] = a[i];
+        }
+
+        if (dpdown[ans2 - 1] < a[i]) {
+            dpdown[ans2++] = a[i];
+        } else {
+            int p = lower_bound(dpdown, dpdown + ans2, a[i]) - dpdown;
+            dpdown[p] = a[i];
+        }
+    }
+    printf("%d\n%d\n", ans1, ans2);
+    return 0;
+}
