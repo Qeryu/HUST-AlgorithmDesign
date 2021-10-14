ef50550a88c19b6e3a590ff495e193540fdee559
Qeryu <m15265723639@163.com>
2021-10-14 14:20:20 +0800

    导弹拦截-添加第二问贪心的具体思路

--- classPractice/luogu1020_1.cpp
+++ classPractice/luogu1020_1.cpp
@@ -20,13 +20,17 @@ int main() {
             dpup[p] = a[i];
         }
 
+        // 贪心解法处理第二问，每次新来一个导弹，就向前找一个当前高度最低且能满足条件的系统来拦截
         int updateSystem = 0;
         for (int j = 1; j <= sumSystem; ++j) {
+            // 判断系统能否拦截该导弹
             if (endSystem[j] >= a[i]) {
+                // 找到高度最低且能拦截的系统作为拦截系统
                 if (!updateSystem || endSystem[j] < endSystem[updateSystem])
                     updateSystem = j;
             }
         }
+        // 找不到就新开一个系统来拦截
         if (!updateSystem)
             endSystem[++sumSystem] = a[i];
         else
