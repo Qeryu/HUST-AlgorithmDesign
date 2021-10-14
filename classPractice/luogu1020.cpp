aa7aab39cf3e48ac06693e121acfe3992168a148
Qeryu <m15265723639@163.com>
2021-10-14 15:34:40 +0800

    导弹拦截-简化LIS写法

--- classPractice/luogu1020.cpp
+++ classPractice/luogu1020.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 using namespace std;
 #define N 100005
-int n, a[N], dpup[N], dpdown[N], ans1, ans2;
+int n, a[N], dpup[N], dpdown[N], ans1, ans2, p;
 // dpup 最长不上升子序列，nlogn做法
 // dpdown 最长上升子序列，nlogn做法
 
@@ -12,23 +12,15 @@ int main() {
     while (scanf("%d", a + (n++)) != EOF)
         ;
     n--;
-    dpup[ans1++] = a[0];
-    dpdown[ans2++] = a[0];
-    for (int i = 1; i < n; ++i) {
-        if (dpup[ans1 - 1] >= a[i]) {
-            dpup[ans1++] = a[i];
-        } else {
-            int p = upper_bound(dpup, dpup + ans1, a[i], greater<int>()) - dpup;
-            dpup[p] = a[i];
-        }
+    for (int i = 0; i < n; ++i) {
+        p = upper_bound(dpup, dpup + ans1, a[i], greater<int>()) - dpup;
+        dpup[p] = a[i];
+        ans1 = max(ans1, p + 1);
 
         // 由DilWorth定理可以确定，覆盖全序列需要的不上升子序列的数量等于最大上升子序列长度
-        if (dpdown[ans2 - 1] < a[i]) {
-            dpdown[ans2++] = a[i];
-        } else {
-            int p = lower_bound(dpdown, dpdown + ans2, a[i]) - dpdown;
-            dpdown[p] = a[i];
-        }
+        p = lower_bound(dpdown, dpdown + ans2, a[i]) - dpdown;
+        dpdown[p] = a[i];
+        ans2 = max(ans2, p + 1);
     }
     printf("%d\n%d\n", ans1, ans2);
     return 0;
