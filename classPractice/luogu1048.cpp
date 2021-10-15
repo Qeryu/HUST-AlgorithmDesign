638e1d428b17b6ca0af9e59881716ba2382cc68a
Qeryu <m15265723639@163.com>
2021-10-15 16:08:29 +0800

    采药-01背包模板题

---
+++ classPractice/luogu1048.cpp
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <cstdio>
+using namespace std;
+const int T = 1e3 + 7;
+int dp[T], t, m, ct, cv;
+
+int main() {
+    freopen("../test.in", "r", stdin);
+    scanf("%d%d", &t, &m);
+    for (int i = 1; i <= m; ++i) {
+        scanf("%d%d", &ct, &cv);
+        for (int j = t; j >= 0; --j) {
+            if (j >= ct) dp[j] = max(dp[j], dp[j - ct] + cv);
+        }
+    }
+    printf("%d\n", dp[t]);
+    return 0;
+}
