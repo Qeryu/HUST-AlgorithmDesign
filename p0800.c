86cb9a31f1f157b305869d9c3bae705afc530446
Qeryu <m15265723639@163.com>
2021-11-10 22:55:00 +0800

    序列最小和-堆的模板题（sk说今晚她要看

---
+++ p0800.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define N 1000007
+
+int n, a[N], b[N];
+
+typedef struct HeapNode {
+    int val, i, j;
+} HeapNode;
+
+int heapSz;
+HeapNode heap[N];
+
+void swim(int p) {
+    int q = p >> 1;
+    HeapNode a = heap[p];
+    while (q && a.val < heap[q].val) {
+        heap[p] = heap[q];
+        p = q;
+        q = p >> 1;
+    }
+    heap[p] = a;
+}
+
+void insertHeap(int x, int i, int j) {
+    heapSz++;
+    heap[heapSz].val = x;
+    heap[heapSz].i = i;
+    heap[heapSz].j = j;
+    swim(heapSz);
+}
+
+void sink(int p) {
+    int q = p << 1;
+    HeapNode a = heap[p];
+    while (q <= heapSz) {
+        if (q < heapSz && heap[q + 1].val < heap[q].val) q++;
+        if (heap[q].val >= a.val) break;
+        heap[p] = heap[q];
+        p = q;
+        q = p << 1;
+    }
+    heap[p] = a;
+}
+
+HeapNode getHeapTop() {
+    HeapNode r = heap[1];
+    heap[1] = heap[heapSz--];
+    sink(1);
+    return r;
+}
+
+int cmpFunc(const void* a, const void* b) {
+    return (*(int*)a - *(int*)b);
+}
+
+int main() {
+    // freopen("test.in", "r", stdin);
+    scanf("%d", &n);
+    for (int i = 0; i < n; ++i) scanf("%d", a + i);
+    for (int i = 0; i < n; ++i) scanf("%d", b + i);
+    qsort(b, n, sizeof(int), cmpFunc);
+
+    for (int i = 0; i < n; ++i) {
+        insertHeap(a[i] + b[0], i, 0);
+    }
+
+    for (int i = 0; i < n; ++i) {
+        HeapNode tmp = getHeapTop();
+        printf("%d ", tmp.val);
+        insertHeap(a[tmp.i] + b[tmp.j + 1], tmp.i, tmp.j + 1);
+    }
+    return 0;
+}
