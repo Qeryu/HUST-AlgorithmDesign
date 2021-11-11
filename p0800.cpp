9a2c4c1233de702b8adbf457da00d4298a9382b3
Qeryu <m15265723639@163.com>
2021-11-11 18:53:32 +0800

    序列最小和-C++写法，简单很多

---
+++ p0800.cpp
@@ -0,0 +1,33 @@
+#include <algorithm>
+#include <cstdio>
+#include <queue>
+using namespace std;
+
+const int N = 1e6 + 7;
+int n, a[N], b[N];
+
+struct HeapNode {
+    int val, i, j;
+    HeapNode(int val, int i, int j) : val(val), i(i), j(j) {}
+    bool operator<(const HeapNode& a) const { return val > a.val; }
+};
+
+priority_queue<HeapNode> heap;
+
+int main() {
+    // freopen("test.in", "r", stdin);
+    scanf("%d", &n);
+    for (int i = 0; i < n; ++i) scanf("%d", a + i);
+    for (int i = 0; i < n; ++i) scanf("%d", b + i);
+    sort(b, b + n);
+
+    for (int i = 0; i < n; ++i) heap.push(HeapNode(a[i] + b[0], i, 0));
+    for (int i = 0; i < n; ++i) {
+        HeapNode top = heap.top();
+        heap.pop();
+        printf("%d ", top.val);
+        heap.push(HeapNode(a[top.i] + b[top.j + 1], top.i, top.j + 1));
+    }
+
+    return 0;
+}
